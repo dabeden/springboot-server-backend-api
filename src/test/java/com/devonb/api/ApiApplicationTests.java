7b027e451ba2438333983b703f80a7a9fe76cfda
dabeden <dabeden@gmail.com>
2026-05-09 14:17:39 -0400

    Initial Spring Boot REST API setup

---
+++ src/test/java/com/devonb/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devonb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
